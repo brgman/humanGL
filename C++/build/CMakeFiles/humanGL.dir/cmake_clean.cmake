870e4ff5c6526b77ad5f3a79a77415289b24e74b
abergman <abergman@student.42.fr>
2026-09-24 08:48:51 +0200

    feat: added alrernative project in c++

---
+++ C++/build/CMakeFiles/humanGL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/humanGL.dir/link.d"
+  "CMakeFiles/humanGL.dir/src/main.cpp.o"
+  "CMakeFiles/humanGL.dir/src/main.cpp.o.d"
+  "humanGL"
+  "humanGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/humanGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
